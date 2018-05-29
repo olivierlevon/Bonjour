3cf2082911baeb278e133dddde37b33e7143e433
olivier <git@levon.org>
2018-05-29 12:39:53 +0200

    remove unneeded header

--- mDNSWindows/mDNSWin32.h
+++ mDNSWindows/mDNSWin32.h
@@ -1,6 +1,6 @@
 /* -*- Mode: C; tab-width: 4 -*-
  *
- * Copyright (c) 2002-2004 Apple Computer, Inc. All rights reserved.
+ * Copyright (c) 2002-2018 Apple Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,8 +16,6 @@
  */
 
 #pragma once
-
-#include	"CommonServices.h"
 
 #include	<mswsock.h>
 
