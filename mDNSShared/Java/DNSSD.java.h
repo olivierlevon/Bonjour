5fa9b81851ad13ff939400c9eca4fffb74275592
olivier <git@levon.org>
2019-05-05 18:03:39 +0200

    add global header for Java 9 and higher

---
+++ mDNSShared/Java/DNSSD.java.h
@@ -0,0 +1,12 @@
+// DNSSD.java.h
+// for Java 9 and higher support
+
+#include "com_apple_dnssd_AppleResolver.h"
+#include "com_apple_dnssd_AppleService.h"
+#include "com_apple_dnssd_AppleBrowser.h"
+#include "com_apple_dnssd_AppleDNSRecord.h"
+#include "com_apple_dnssd_AppleDNSSD.h"
+#include "com_apple_dnssd_AppleDomainEnum.h"
+#include "com_apple_dnssd_AppleQuery.h"
+#include "com_apple_dnssd_AppleRecordRegistrar.h"
+#include "com_apple_dnssd_AppleRegistration.h"
